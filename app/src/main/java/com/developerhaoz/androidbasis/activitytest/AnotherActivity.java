ec9c4c75a0c45e0cf04bca4e4a4be7d00a28375a
Haoz <developerhaon@gmail.com>
2018-01-08 23:26:38 +0800

    Activity 相关复习 finish.

--- app/src/main/java/com/developerhaoz/androidbasis/activitytest/AnotherActivity.java
+++ app/src/main/java/com/developerhaoz/androidbasis/activitytest/AnotherActivity.java
@@ -1,15 +1,76 @@
 package com.developerhaoz.androidbasis.activitytest;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.developerhaoz.androidbasis.R;
 
 public class AnotherActivity extends AppCompatActivity {
 
+    private static final String TAG = "AnotherActivity111";
+
+    public static void startActivity(Context context){
+        Intent intent = new Intent(context, AnotherActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG, "onNewIntent: ");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_another);
+        findViewById(R.id.another_btn_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LifeActivity.startActivity(AnotherActivity.this);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+        Log.d(TAG, "onStart: " + this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
     }
 }
--- app/src/main/java/com/developerhaoz/androidbasis/activitytest/LifeActivity.java
+++ app/src/main/java/com/developerhaoz/androidbasis/activitytest/LifeActivity.java
@@ -1,26 +1,38 @@
 package com.developerhaoz.androidbasis.activitytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.developerhaoz.androidbasis.R;
 
 public class LifeActivity extends AppCompatActivity {
     
     private Button mBtnTest;
-    private static final String TAG = "LifeActivity";
+    private EditText mEtTest;
+    private static final String TAG = "LifeActivity111";
+    private static final String KEY_EDIT = "key_edit";
+
+    public static void startActivity(Context context){
+        Intent intent = new Intent(context, LifeActivity.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life);
+        mEtTest = (EditText) findViewById(R.id.life_et_test);
+        Log.d(TAG, "onCreate: ");
         findViewById(R.id.life_btn_test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                AnotherActivity.startActivity(getApplicationContext());
             }
         });
     }
@@ -29,6 +41,7 @@ public class LifeActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart: ");
+        Log.d(TAG, "onStart: " + this);
     }
 
     @Override
@@ -59,5 +72,24 @@ public class LifeActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: ");
+        String editContent = mEtTest.getText().toString();
+        if(!"".equals(editContent)){
+            outState.putString(KEY_EDIT, editContent);
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: ");
+        if(savedInstanceState != null){
+            mEtTest.setText(savedInstanceState.getString(KEY_EDIT));
+        }
     }
 }
