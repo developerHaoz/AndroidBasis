cbc805d62937da303a0d898ab63187c0f9f4b750
Haoz <developerhaon@gmail.com>
2018-01-12 11:05:46 +0800

    服务相关的一些代码。

--- app/src/main/java/com/developerhaoz/androidbasis/MainActivity.java
+++ app/src/main/java/com/developerhaoz/androidbasis/MainActivity.java
@@ -1,9 +1,15 @@
 package com.developerhaoz.androidbasis;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> resultList = new ArrayList<>();
+    int[] result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
