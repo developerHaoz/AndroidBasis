83e5abded153e991192166320c563da485cf843f
Haoz <developerhaon@gmail.com>
2018-01-18 17:45:45 +0800

    一些小修改。

---
+++ app/src/main/java/com/developerhaoz/androidbasis/asynctasktest/AsyncTaskActivity.java
@@ -0,0 +1,15 @@
+package com.developerhaoz.androidbasis.asynctasktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.developerhaoz.androidbasis.R;
+
+public class AsyncTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task);
+    }
+}
