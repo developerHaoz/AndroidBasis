ebc0b2f7edcb2c254fdbcd506d8c8e125fa8129a
Haoz <developerhaon@gmail.com>
2018-03-20 20:27:48 +0800

    今天的效率还可以，看完了一部分头条的 Android 面试题，明后两天的笔试加油。

--- app/src/main/java/com/developerhaoz/androidbasis/MainActivity.java
+++ app/src/main/java/com/developerhaoz/androidbasis/MainActivity.java
@@ -2,18 +2,79 @@ package com.developerhaoz.androidbasis;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<String> resultList = new ArrayList<>();
-    int[] result;
+    private static final String TAG = "MainActivity111";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
