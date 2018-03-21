5dee92a8f6c5abed8701f05a7f68d89f2497b9e3
Haoz <developerhaon@gmail.com>
2018-03-21 20:50:39 +0800

    看完头条大部分的面试题，发现自己确实还有很大的提升空间。

---
+++ app/src/main/java/com/developerhaoz/androidbasis/servicetest/TestClass.java
@@ -0,0 +1,17 @@
+package com.developerhaoz.androidbasis.servicetest;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * @author Haoz
+ * @date 2018/3/21.
+ */
+public class TestClass {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String inputContent = in.nextLine();
+        String str = "sdfsf sdf sdf";
+        HashMap<String, Integer> hashMap = new HashMap();
+    }
+}
