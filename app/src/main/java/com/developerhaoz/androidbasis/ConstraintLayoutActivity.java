a399285ce523d6c6b2cf4b828f230c8a549231e9
Haoz <developerhaon@gmail.com>
2018-03-25 21:20:53 +0800

    又是一个纠结的一天。

--- app/src/main/java/com/developerhaoz/androidbasis/ConstraintLayoutActivity.java
+++ app/src/main/java/com/developerhaoz/androidbasis/ConstraintLayoutActivity.java
@@ -2,7 +2,6 @@ package com.developerhaoz.androidbasis;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class ConstraintLayoutActivity extends AppCompatActivity {
 
@@ -10,8 +9,6 @@ public class ConstraintLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
